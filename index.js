bc87049248b55412ef50ea86496f70e769f593f6
praizjosh <praizjosh@outlook.com>
2024-09-09 22:44:53 +0100

    commit

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
-// index.js
-// where your node app starts
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // init project
-var express = require('express');
+import express from "express";
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+import cors from "cors";
+
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
-
-// your first API endpoint... 
-// app.get("/api/hello", function (req, res) {
-//   res.json({greeting: 'hello API'});
-// });
-
-app.get('/api/:date?', (req, res) => {
-  let date = req.params.date ? new Date(req.params.date) : new Date();
-
-  if (/\d{5,}/.test(req.params.date)) {
-    date = new Date(parseInt(req.params.date));
-  }
-
-  if (date.toString() === 'Invalid Date') {
-    return res.json({ error: 'Invalid Date' });
-  }
-
-  res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
+// Transform Date API endpoint...
+app.get("/api/:date?", function (req, res) {
+  const dateParam = req.params.date;
+  const isValidISODate = /^\d{4}-\d{2}-\d{2}$/.test(dateParam);
+
+  const date = !dateParam
+    ? new Date() // No date parameter provided
+    : /^\d+$/.test(dateParam)
+    ? new Date(parseInt(dateParam)) // Unix timestamp
+    : isValidISODate // isValid date format
+    ? new Date(dateParam) // ISO format
+    : null; // Invalid format
+
+  // Validate the date
+  if (!date || isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
